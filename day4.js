bddb277892c1f815ee97cc647130f58a668afd33
Tristan Carel <tcarel@umass.edu>
2022-12-04 01:02:00 -0500

    cleaned up boolean logic on day 4

--- day4.js
+++ day4.js
@@ -5,26 +5,18 @@ function parser(){
     return contents.split(/[.\s]+/);
 }
 
-
-function getRangeAsArr(values) {
-    let list = [];
-    for (let i = values[0]; i <= values[1]; ++i){
-        list.push(i);
-    }
-    return list;
-}
 function day4Part1(input) {
     input.pop();
     let ints = input.map((e) => e.split(','));
-    ints = ints.map((value) => value.map((curElem) =>getRangeAsArr(curElem.split('-').map((number) => parseInt(number)))));
-    return ints.reduce((acc, e) => e[0][0] >= e[1][0] && e[0][e[0].length - 1] <= e[1][e[1].length - 1] || e[0][0] <= e[1][0] && e[0][e[0].length - 1] >= e[1][e[1].length - 1] ? acc + 1 : acc, 0);
+    ints = ints.map((value) => value.map((curElem) => curElem.split('-').map((number) => parseInt(number))));
+    return ints.reduce((acc, e) => e[0][0] >= e[1][0] && e[0][1] <= e[1][1] || e[0][0] <= e[1][0] && e[0][1] >= e[1][1] ? acc + 1 : acc, 0);
 }
 
 function day4Part2(input) {
     input.pop();
     let ints = input.map((e) => e.split(','));
-    ints = ints.map((value) => value.map((curElem) =>getRangeAsArr(curElem.split('-').map((number) => parseInt(number)))));
-    return ints.reduce((acc, e) => e[0].reduce((isOverlap, element) => e[1].findIndex((num) => num === element) !== -1 ? true : isOverlap, false)? acc + 1 : acc, 0);
+    ints = ints.map((value) => value.map((curElem) => curElem.split('-').map((number) => parseInt(number))));
+    return ints.reduce((acc, e) => e[0][1] >= e[1][0] && e[1][1] >= e[0][0] ? acc += 1 : acc, 0);
 }
 
 
